33b35d107d61ca4111ccc00d1c3b80f2e4947a0c
Alberto Benatti <albi.benni8@gmail.com>
2025-04-22 15:52:53 +0200

    wip main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+mod hosts;
+mod utils;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Let's Relax!");
+    let arguments: Vec<String> = std::env::args().map(|arg| arg).collect();
+    if arguments.len() < 1 {
+        panic!("arguments must be defined");
+    }
+    if arguments
+        .get(1)
+        .expect("No arguments provided")
+        .to_lowercase()
+        == "help"
+    {
+        utils::utils::help();
+    }
 }
