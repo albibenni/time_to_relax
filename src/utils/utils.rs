b944375eb9a84ba769e8ec893dad18e6b4c26b54
Alberto Benatti <albi.benni8@gmail.com>
2025-04-22 15:53:10 +0200

    utils

---
+++ src/utils/utils.rs
@@ -0,0 +1,11 @@
+pub fn help() -> () {
+    println!("Provide as many arguments as you want of those supported: ");
+    // println!("  - {} ", YOUTUBE);
+    // println!("  - {} ", X);
+    // println!("  - {} ", NETFLIX);
+    // println!("Provide a preset of those supported: ");
+    // println!("  - {} ", ALL);
+    // println!("  - {} ", CODING);
+    // println!("  - {} ", STUDYING);
+    println!("As last argument provide the Pomodoro timer in minutes until then the websites are blocked");
+}
