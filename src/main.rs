57746a4147bf3df1fe145e9fae546419627d24c6
Alberto Benatti <albi.benni8@gmail.com>
2025-04-28 18:08:09 +0200

    wip args

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 mod hosts;
 mod utils;
 
-fn main() {
+const RESET_FILE_PATH: &str = "
+##
+# Host Database
+#
+# localhost is used to configure the loopback interface
+# when the system is booting.  Do not change this entry.
+##
+127.0.0.1		localhost
+255.255.255.255		broadcasthost
+::1                          localhost
+127.0.0.1		www.youtube.com
+";
+const FILE_PATH: &str = "/etc/hosts";
+//const LOCALHOST: &str = "127.0.0.1";
+
+fn main() -> Result<(), std::io::Error> {
     println!("Let's Relax!");
     let arguments: Vec<String> = std::env::args().map(|arg| arg).collect();
     if arguments.len() < 1 {
@@ -15,4 +30,10 @@ fn main() {
     {
         utils::utils::help();
     }
+    let res_flux = utils::utils::execute_flux_cache();
+    if res_flux.is_err() {
+        return res_flux;
+    }
+    //utils::utils::plan_sleep(sleep_time);
+    return utils::utils::reset_file(RESET_FILE_PATH);
 }
